6caf91b42942fc2aa69e5b500cb4c1c2ab67d026
Vicky <ff9848803577@gmail.com>
2026-05-31 12:12:00 +0545

    Added new files and changes

---
+++ problems/problem10.py
@@ -0,0 +1,11 @@
+# WAP to create a dictionary of nepali words with thir values as their Eng  trnslation. Provide user option to look it up!
+
+words = {
+    "Bikki": "Killer",
+    "Amar": "chair",
+    "Singhe": "Doke",
+}
+
+word = input(" Enter the word you want meaning of:")
+
+print(words[word])
