d2b111042759ddea1b94f5838427bccd1bb762c4
Vicky <ff9848803577@gmail.com>
2026-06-01 20:46:29 +0545

    Added new files and changes

---
+++ conditional.py
@@ -0,0 +1,10 @@
+a = int(input("Enter your age:"))
+
+if(a>18):
+    print("You are eligible to vote")
+    print("Congrats!!")
+
+else:
+    print("Your are not eligible to vote")
+    print("Sorry!!")
+    
